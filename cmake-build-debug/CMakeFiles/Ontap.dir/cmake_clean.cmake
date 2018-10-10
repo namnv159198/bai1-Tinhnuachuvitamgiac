87bbc1acfe300636e9e929c353b18121cff0aa7a
namnvth1809002 <namnvth1809002@fpt.edu.vn>
2018-10-10 18:52:52 +0700

    Tinh nua chi vi tam giac

---
+++ cmake-build-debug/CMakeFiles/Ontap.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ontap.dir/main.c.obj"
+  "Ontap.pdb"
+  "Ontap.exe"
+  "Ontap.exe.manifest"
+  "libOntap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ontap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+float Tinhnuachuvitamgiac(int a,int b,int c){
+
+    return  (float) (a+b+c)/2;
+}
+int main(){
+    int a,b,c;
+    float nuachuvi;
+    printf(" Nhap ba canh cua tam giac abc : \n");
+    printf("canh a: \n");
+    scanf("%d",&a);
+    printf("canh b: \n");
+    scanf("%d",&b);
+    printf("canh c: \n");
+    scanf("%d",&c);
+    if (a <0 || b < 0 || c < 0){
+        printf("Do dai cua mot canh tam giac khong the nho hon 0.\n");
+    }
+    if (a+b <= c || a+c <=b || b+c <=a){
+        printf("Tong hai canh tam giac luon lon hon canh con lai.\n");
+    }
+    else{
+       float  nuachuvi = Tinhnuachuvitamgiac(a,b,c);
+        printf("Nua chu vi cua tam giac abc la: \n");
+        printf("%.2f",nuachuvi);
+    }
+}
